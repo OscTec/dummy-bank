90759fb9bc67bc1e5e14b6afabde0befcf07e3b8
Oscar <osctec@hotmail.co.uk>
2023-03-26 16:58:55 +0100

    Add cards to dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 
+interface Notification {
+  id: string;
+  title: string;
+  description: string;
+}
+
 interface Props {
-  notifications: number
+  notifications: Notification[]
 }
 
 const Navbar = ({ notifications }: Props) => {
@@ -25,7 +31,7 @@ const Navbar = ({ notifications }: Props) => {
         <button className="btn btn-ghost btn-circle">
           <div className="indicator">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-            {notifications > 0 && <span className="badge badge-xs badge-primary indicator-item"></span>}
+            {notifications.length > 0 && <span className="badge badge-xs badge-primary indicator-item"></span>}
           </div>
         </button>
       </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,16 @@
+import Card from "../components/Card"
 import Navbar from "../components/Navbar"
 
 const Dashboard = () => {
   return (
     <div style={{ backgroundColor: '#f2f2f2', height: '100vh' }} className="drop-shadow-2xl">
-      <Navbar />
+      <Navbar notifications={[]} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center pt-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   )
 }
