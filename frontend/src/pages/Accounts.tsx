589d507a332449e11869876710bbd96029ce8088
Oscar <osctec@hotmail.co.uk>
2023-04-01 21:32:16 +0100

    Fix: skeleton staying after loading

--- frontend/src/pages/Accounts.tsx
+++ frontend/src/pages/Accounts.tsx
@@ -7,7 +7,7 @@ const Accounts = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center pt-4">
-      {true && [1, 2].map((i) => <AccountSkeleton key={i} />)}
+      {isLoading && [1, 2].map((i) => <AccountSkeleton key={i} />)}
       {error && <div>Something went wrong</div>}
       {data && data.map(account => (
         <Account key={account.id} accountType={account.type} balance={account.balance} />
