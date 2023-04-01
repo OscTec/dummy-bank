d1f2f1e1ea92b2f4b9342031fea01e3cb3698976
Oscar <osctec@hotmail.co.uk>
2023-04-01 10:13:07 +0100

    Refactor names

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 interface Notification {
   id: string;
@@ -18,14 +19,13 @@ const Navbar = ({ notifications }: Props) => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Homepage</a></li>
-            <li><a>Portfolio</a></li>
-            <li><a>About</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><a>Log Out</a></li>
           </ul>
         </div>
       </div>
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">Quark Bank</Link>
       </div>
       <div className="flex-none">
         <button className="btn btn-ghost btn-circle">
