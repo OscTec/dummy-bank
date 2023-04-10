8911679b092dbf2a507b41225ad224f839520257
Oscar <osctec@hotmail.co.uk>
2023-04-10 22:03:58 +0100

    Install jwt-decode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import jwtDecode from 'jwt-decode'
 
-import Dashboard from './pages/Dashboard'
 import Token from './types/Token'
 
 import './App.css'
-import { Outlet } from 'react-router-dom'
 
 function App() {
   const [user, setUser] = useState<Token | null>(null)
@@ -14,7 +13,6 @@ function App() {
     const token = localStorage.getItem('token')
     if (token) {
       const decodedToken = jwtDecode<Token>(token)
-      console.log(decodedToken)
       setUser(decodedToken)
     }
   }, [])
